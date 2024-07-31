be2c6e580ffe1d5bf2a69420f0dd889c99de2792
Samuel Maiko <sammymaiko721@gmail.com>
2024-07-31 16:50:12 +0300

    error fixes - emoji storage issue

--- a_profile/models/Profile.py
+++ a_profile/models/Profile.py
@@ -1,7 +1,6 @@
+from api.models import BaseModel
 from django.conf import settings
 from django.db import models
-
-from api.models import BaseModel
 
 
 class Profile(BaseModel):
--- core/settings.py
+++ core/settings.py
@@ -89,18 +89,22 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
     'default': {
-             'ENGINE': env('DATABASE_ENGINE'),
-             'NAME': env('DATABASE_NAME'),
-             'USER': env('DATABASE_USER'),
-             'PASSWORD': env('DATABASE_PASSWORD'),
-             'HOST': env('DATABASE_HOST'),
-             'PORT': env('DATABASE_PORT'),
-         }
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+    # 'default': {
+    #          'ENGINE': env('DATABASE_ENGINE'),
+    #          'NAME': env('DATABASE_NAME'),
+    #          'USER': env('DATABASE_USER'),
+    #          'PASSWORD': env('DATABASE_PASSWORD'),
+    #          'HOST': env('DATABASE_HOST'),
+    #          'PORT': env('DATABASE_PORT'),
+    #          'OPTIONS': {
+    #               'charset': 'utf8mb4',
+    #               'init_command': "SET sql_mode='STRICT_TRANS_TABLES', innodb_strict_mode=1",
+#               },
+    #      }
 }
 
 AUTH_PASSWORD_VALIDATORS = [
