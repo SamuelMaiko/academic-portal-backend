ee8707b933920b105220a32b33b3b38123e0e1df
Samuel Maiko <sammymaiko721@gmail.com>
2024-07-31 16:58:11 +0300

    error fixes - emoji storage issue

--- core/settings.py
+++ core/settings.py
@@ -100,10 +100,10 @@ DATABASES = {
              'PASSWORD': env('DATABASE_PASSWORD'),
              'HOST': env('DATABASE_HOST'),
              'PORT': env('DATABASE_PORT'),
-             'OPTIONS': {
-                  'charset': 'utf8mb4',
-                  'init_command': "SET sql_mode='STRICT_TRANS_TABLES', innodb_strict_mode=1",
-              },
+            #  'OPTIONS': {
+            #       'charset': 'utf8mb4',
+            #       'init_command': "SET sql_mode='STRICT_TRANS_TABLES', innodb_strict_mode=1",
+            #   },
          }
 }
 
